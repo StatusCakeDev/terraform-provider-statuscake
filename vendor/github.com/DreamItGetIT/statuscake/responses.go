09f331dcd211b2e40e0899bca6474e660317a4de
jmherbst <jmherbst@gmail.com>
2018-01-10 09:47:04 -0500

    Updating DreamItGetIT statuscake vendored client

--- vendor/github.com/DreamItGetIT/statuscake/responses.go
+++ vendor/github.com/DreamItGetIT/statuscake/responses.go
@@ -28,7 +28,7 @@ type detailResponse struct {
 	Paused          bool     `json:"Paused"`
 	WebsiteName     string   `json:"WebsiteName"`
 	URI             string   `json:"URI"`
-	ContactID       int      `json:"ContactID"`
+	ContactID       string   `json:"ContactID"`
 	Status          string   `json:"Status"`
 	Uptime          float64  `json:"Uptime"`
 	CustomHeader    string   `json:"CustomHeader"`
